e1ef4f97c6a85c8c97ca196af81a78f7ec0ab951
teprinciple <teprinciple@foxmail.com>
2019-08-08 16:56:47 +0800

    mailsender 1.1.0

--- app/src/main/java/teprinciple/yang/sendmaildemo/MainActivity.kt
+++ app/src/main/java/teprinciple/yang/sendmaildemo/MainActivity.kt
@@ -1,74 +1,96 @@
 package teprinciple.yang.sendmaildemo
 
+import android.graphics.Color
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import android.view.View
-import android.widget.EditText
 import android.widget.Toast
 import com.teprinciple.mailsender.Mail
 import com.teprinciple.mailsender.MailSender
+import kotlinx.android.synthetic.main.activity_main.*
 import java.io.*
 
-class MainActivity : AppCompatActivity() {
-
-    private var editText: EditText? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        editText = findViewById(R.id.toAddEt)
-    }
-
+class MainActivity : AppCompatActivity(), MailSender.OnMailSendListener {
 
     private val mail by lazy {
         Mail().apply {
-            mailServerHost = "smtp.qq.com"
-            mailServerPort = "587"
-            fromAddress = "xxx@foxmail.com" // 替换成你的发件箱
-            password = "xxxx" // 替换成你的发件箱授权码
-            toAddress = arrayListOf("xxxx@qq.com") // 替换成你的收件箱
 
+            // 发件箱服务器地址
+            mailServerHost = etServerHost.text.toString()
+            // 发件箱服务器端口
+            mailServerPort = etServerHostPort.text.toString()
+            // 发件邮箱地址
+            fromAddress = etFromAddress.text.toString()
+            // 发件邮箱授权码
+            password = etFromPwd.text.toString()
+            // 收件箱列表
+            toAddress = arrayListOf(etToAddress.text.toString())
+            // 邮件主题
             subject = "MailSender测试邮件"
-//            content = "这是一个测试邮件"
+
+            // 发送纯文本内容的邮件
+            // content = "MailSender Android快速实现发送邮件"
+
+            // 发送html内容的邮件
+            /*
             content = """
                 <p1 style = "color: red">MailSender</p1><br/>
                 <p1 style = "color: blue">Android快速实现发送邮件</p1><br/>
                 <p1 style = "color: blue">https://github.com/teprinciple/MailSender</p1><br/>
-	            <img src="https://avatars2.githubusercontent.com/u/19629464?s=460&v=4">
+                <p6 style = "color: gray">这是html内容的邮件</p1><br/>
+	            <img src="https://upload.jianshu.io/users/upload_avatars/2368611/5248865afe92.jpg?imageMogr2/auto-orient/strip|imageView2/1/w/240/h/240">
             """
+            */
+
+            // 发送SpannableString内容的邮件
+            content = SpanUtils(this@MainActivity)
+                .appendLine("MailSender").setFontSize(28, true).setForegroundColor(Color.RED)
+                .appendLine("Android快速实现发送邮件")
+                .appendLine("https://github.com/teprinciple/MailSender").setForegroundColor(Color.BLUE)
+                .appendLine("这是SpannableString内容的邮件")
+                .setForegroundColor(Color.parseColor("#888888")).setFontSize(12, true)
+                .create()
         }
     }
 
-    fun senTextMail(view: View) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
 
-        val file = File(getExternalFilesDir("file").absolutePath + File.separator + "test.txt")
-        var os: OutputStream? = null
-        try {
-            os = FileOutputStream(file)
-            val str = "hello world"
-            val data = str.toByteArray()
-            os.write(data)
-        } catch (e: FileNotFoundException) {
-            e.printStackTrace()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } finally {
-            try {
-                os?.close()
-            } catch (e: IOException) {
-            }
+        // 发送普通邮件
+        btnSendMail.setOnClickListener {
+            MailSender.getInstance().sendMail(mail, this)
         }
 
-        mail.attachFiles = arrayListOf(file)
+        // 发送带附件的邮件
+        btnSendMailWithFile.setOnClickListener {
 
-        MailSender.getInstance().sendMail(mail,object : MailSender.OnMailSendListener{
-            override fun onSuccess() {
-                //Toast.makeText(this@MainActivity,"发送成功",Toast.LENGTH_SHORT).show()
+            val file = File(getExternalFilesDir("file").absolutePath + File.separator + "MailSender.txt")
+            var os: OutputStream? = null
+            try {
+                os = FileOutputStream(file)
+                val str = "hello world"
+                val data = str.toByteArray()
+                os.write(data)
+            } catch (e: FileNotFoundException) {
+                e.printStackTrace()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            } finally {
+                try {
+                    os?.close()
+                } catch (e: IOException) {
+                }
             }
+            mail.attachFiles = arrayListOf(file)
+            MailSender.getInstance().sendMail(mail, this)
+        }
+    }
 
-            override fun onError(message: String?) {
-                //Toast.makeText(this@MainActivity,"发送失败: $message",Toast.LENGTH_SHORT).show()
-            }
-        })
+    override fun onSuccess() {
+        Toast.makeText(this@MainActivity, "发送成功", Toast.LENGTH_SHORT).show()
+    }
+
+    override fun onError(e: Throwable) {
+        Toast.makeText(this@MainActivity, "发送失败: $e.message", Toast.LENGTH_SHORT).show()
     }
 }
